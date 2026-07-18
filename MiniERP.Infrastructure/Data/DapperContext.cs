9a7721f2d3b962020ca5fab5da97814c37e5f223
Piumi Kavindya <piyumikavindyappk@gmail.com>
2026-07-18 18:58:43 +0530

    add repository class

--- MiniERP.Infrastructure/Data/DapperContext.cs
+++ MiniERP.Infrastructure/Data/DapperContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Data.SqlClient;
+using MiniERP.Core.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace MiniERP.Infrastructure.Data
 {
