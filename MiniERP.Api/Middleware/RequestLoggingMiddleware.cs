ef2eba0f9b876a3366f10921a42a4297251a40a6
Piumi Kavindya <piyumikavindyappk@gmail.com>
2026-08-28 14:31:31 +0530

    Inject ILogger to Custom Middleware

--- MiniERP.Api/Middleware/RequestLoggingMiddleware.cs
+++ MiniERP.Api/Middleware/RequestLoggingMiddleware.cs
@@ -3,21 +3,34 @@
     public class RequestLoggingMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
 
-        public RequestLoggingMiddleware(RequestDelegate next)
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            Console.WriteLine(
+            /*Console.WriteLine(
             $"Request started: {context.Request.Method} {context.Request.Path}");
 
             await _next(context);
 
             Console.WriteLine(
-                $"Request completed: {context.Response.StatusCode}");
+                $"Request completed: {context.Response.StatusCode}");*/
+
+            _logger.LogInformation(
+          "Request started: {Method} {Path}",
+          context.Request.Method,
+          context.Request.Path);
+
+            await _next(context);
+
+            _logger.LogInformation(
+                "Request completed with status code: {StatusCode}",
+                context.Response.StatusCode);
         }
     }
 }
