e0f9e34077b3f8e794931148f9397d441fe30d79
Piumi Kavindya <piyumikavindyappk@gmail.com>
2026-09-15 13:35:16 +0530

    created ExceptionHandlingMiddleware

---
+++ MiniERP.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace MiniERP.Api.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception occurred.");
+
+                context.Response.StatusCode =
+                    (int)HttpStatusCode.InternalServerError;
+
+                context.Response.ContentType = "application/json";
+
+                var response = new
+                {
+                    Message = "An unexpected error occurred."
+                };
+
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(response));
+            }
+        }
+    }
+}
