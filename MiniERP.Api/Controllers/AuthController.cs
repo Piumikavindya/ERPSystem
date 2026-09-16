83231e9a0383f8231c4724c88c54b9d437d85015
Piumi Kavindya <piyumikavindyappk@gmail.com>
2026-09-16 15:41:43 +0530

    testing controller

--- MiniERP.Api/Controllers/AuthController.cs
+++ MiniERP.Api/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using MiniERP.Core.Models;
 
 namespace MiniERP.Api.Controllers
 {
@@ -7,6 +9,20 @@ namespace MiniERP.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-
+        [HttpPost("login")]
+        public IActionResult Login(LoginModel model)
+        {
+            if(model.UserName == "admin" && model.Password == "12345")
+            {
+                return Ok(new
+                { 
+                    Message = "Login Successfully"
+                });
+            }
+            return Unauthorized(new
+            {
+                Message = "Invalid username or password"
+            });
+        }
     }
 }
